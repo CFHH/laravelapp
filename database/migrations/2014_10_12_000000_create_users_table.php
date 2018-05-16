4539d8c30b702897df4cf636a680820cd1fee00f
CFHH <34704169+CFHH@users.noreply.github.com>
2018-05-16 11:52:49 +0800

    为了mysql自动分区，做的事情
    用CRC把email变成64位整数（具体看php的版本，windows平台下是32位）

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        /*
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
@@ -21,6 +22,22 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        */
+
+        $sql = <<<EOT
+CREATE TABLE users (
+    id_crc64 BIGINT PRIMARY KEY UNIQUE,
+    email VARCHAR(50),
+    name VARCHAR(20) NOT NULL,
+    password VARCHAR(2555),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
+ENGINE=INNODB
+PARTITION BY HASH(id_crc64)
+PARTITIONS 3;
+EOT;
+        DB::statement($sql);
     }
 
     /**
